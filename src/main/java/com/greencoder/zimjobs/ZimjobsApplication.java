85e28ea8b4a41e8cb1450b1a7b7c761d5d4ed364
Takadza <nigeltakadza@gmail.com>
2020-11-15 15:06:46 +0200

    first initalization of the project

---
+++ src/main/java/com/greencoder/zimjobs/ZimjobsApplication.java
@@ -0,0 +1,13 @@
+package com.greencoder.zimjobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZimjobsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZimjobsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/greencoder/zimjobs/ZimjobsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greencoder.zimjobs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZimjobsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
